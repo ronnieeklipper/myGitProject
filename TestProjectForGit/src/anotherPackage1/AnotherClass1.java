4dd119b438ae16229f1f2606a9b867d2ab4bdeac
Rahul <Rahul@LAPTOP-9VGC5NFC>
2021-02-14 08:24:44 -0800

    New Project commit and push

---
+++ TestProjectForGit/src/anotherPackage1/AnotherClass1.java
@@ -0,0 +1,12 @@
+package anotherPackage1;
+
+public class AnotherClass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Do I care");
+
+	}
+
+}
